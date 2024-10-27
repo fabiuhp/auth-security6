cb6f50339c25f015f11f905c6859e6ba7132eafd
Fabio Pereira <fabiuhp@msn.com>
2024-10-26 23:07:34 -0300

    initial

---
+++ src/main/java/br/com/fabiopereira/authSecurity6/AuthSecurity6Application.java
@@ -0,0 +1,13 @@
+package br.com.fabiopereira.authSecurity6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthSecurity6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthSecurity6Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fabiopereira/authSecurity6/AuthSecurity6ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fabiopereira.authSecurity6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSecurity6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
